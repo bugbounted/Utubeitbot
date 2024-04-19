c7bc1281c52542a6ff743bd2fdd052841ac40c70
Mohamad Samardeh Haghighi Fard <samardehmohamad@gmail.com>
2024-04-19 12:34:08 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -33,7 +33,7 @@ class Config:
 
     AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
-    AUTH_USERS = [BOT_OWNER, 754495556] + (
+    AUTH_USERS = [BOT_OWNER, 851576081] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
         if AUTH_USERS_TEXT
         else []
